ae6cf67d8a28681051f747f2cfc7728b2e4eb9b4
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-11-28 12:50:00 +0500

    index file created

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 from flask import Flask, render_template, request
+
+# Creating flask app
+app = Flask(__name__, template_folder='./public')
+
+
+@app.route('/')
+def render():
+    return render_template('index.html')
