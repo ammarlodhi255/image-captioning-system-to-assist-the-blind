18188046300e1359baafc11baf87dbb057c22519
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-11-28 13:05:34 +0500

    functions init_model and process_img defined

--- predict.py
+++ predict.py
@@ -6,3 +6,11 @@ import sys
 import base64
 sys.path.append(os.path.abspath('./model'))
 
+def init_model():
+    loaded_model = tf.keras.modes.load_model('')
+    print('Model Loaded Successfully')
+    return loaded_model
+
+def process_image(img):
+    with open('img_output.png', 'wb') as output:
+        output.write(base64.b64decode(str(img, 'utf-8')))
