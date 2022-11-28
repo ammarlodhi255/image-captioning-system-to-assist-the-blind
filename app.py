f9296110e706e2f7a493cffa0a573c25711dd0be
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-11-28 12:53:20 +0500

    port specified

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
+import os
 
 # Creating flask app
 app = Flask(__name__, template_folder='./public')
 
 
+# Home endpoint, render html file
 @app.route('/')
 def render():
     return render_template('index.html')
+
+# Run app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
