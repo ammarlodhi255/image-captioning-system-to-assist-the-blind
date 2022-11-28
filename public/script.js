ce11681f2dec8cd86eedc4a20fb45b4f5c698266
Filza <filzaahmad3@gmail.com>
2022-11-28 21:18:33 +0500

    fixed image size and other bugs

--- public/script.js
+++ public/script.js
@@ -1,44 +1,43 @@
 //selecting all required elements
-const dropArea = document.querySelector(".drag-area"),
-dragText = dropArea.querySelector("header"),
-button = dropArea.querySelector("button"),
-input = dropArea.querySelector("input");
+const dropArea = document.querySelector(".drag-area")
+dragText = dropArea.querySelector("header")
+button = dropArea.querySelector("button")
+input = dropArea.querySelector("input")
 da = document.querySelector(".drag-area").innerHTML
 let file; //this is a global variable and we'll use it inside multiple functions
 removeImageButton = document.getElementById('rmv-img-btn')
 
-function browseBtnCLicked(){ input.click(); //if user click on the button then the input also clicked
+function browseBtnCLicked(){ document.getElementById('input').click(); //if user clicks on the button then the input is also clicked
 }
 
-input.addEventListener("change", function(){
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-  file = this.files[0];
+function inputChanged(){
+  //getting user select file and [0] this means if user selecst multiple files then we'll select only the first one
+  file = document.getElementById('input').files[0];
   dropArea.classList.add("active");
   showFile(); //calling function
-});
+}
 
 
 //If user Drag File Over DropArea
 function dragOver(event){
   event.preventDefault(); //preventing from default behaviour
   dropArea.classList.add("active");
-  dragText.textContent = "Release to Upload File";
+  dropArea.querySelector("header").textContent = "Release to Upload File";
 }
 
 //If user leave dragged File from DropArea
 function dragLeave(){
   dropArea.classList.remove("active");
-  dragText.textContent = "Drag & Drop to Upload File";
+  dropArea.querySelector("header").textContent = "Drag & Drop to Upload File";
 }
 
 
-//If user drop File on DropArea
-dropArea.addEventListener("drop", (event)=>{
+function dropped(event){
   event.preventDefault(); //preventing from default behaviour
   //getting user select file and [0] this means if user select multiple files then we'll select only the first one
   file = event.dataTransfer.files[0];
   showFile(); //calling function
-});
+}
 
 function showFile(){
   let fileType = file.type; //getting selected file type
@@ -47,7 +46,7 @@ function showFile(){
     let fileReader = new FileReader(); //creating new FileReader object
     fileReader.onload = ()=>{
       let fileURL = fileReader.result; //passing user file source in fileURL variable
-      let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+      let imgTag = `<img src="${fileURL}" alt="image" height=500 width=500>`; //creating an img tag and passing user selected file source inside src attribute
       dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
     }
     fileReader.readAsDataURL(file);
@@ -67,5 +66,5 @@ function rmvButtonClicked(){
   dropArea.innerHTML = da;
   document.getElementById('caption').innerText = '';
   removeImageButton.style.display = "none";
-
+  file = null
 }
