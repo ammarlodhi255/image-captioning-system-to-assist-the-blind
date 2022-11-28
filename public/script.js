4ffeb8be6e372e52b3f7436bc5d01a0d80a11136
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-11-28 23:51:38 +0500

    minor changes

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
-//selecting all required elements
 const dropArea = document.querySelector(".drag-area")
 dragText = dropArea.querySelector("header")
 button = dropArea.querySelector("button")
 input = dropArea.querySelector("input")
 da = document.querySelector(".drag-area").innerHTML
-let file; //this is a global variable and we'll use it inside multiple functions
+let file; 
 removeImageButton = document.getElementById('rmv-img-btn')
 
 function browseBtnCLicked() { 
@@ -37,24 +36,28 @@ function dropped(event) {
   event.preventDefault(); //preventing from default behaviour
   //getting user select file and [0] this means if user select multiple files then we'll select only the first one
   file = event.dataTransfer.files[0];
-  showFile(); //calling function
+  showFile(); 
 }
 
 function showFile() {
   document.getElementById('caption').innerText = ''
-  let fileType = file.type; //getting selected file type
-  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-  if(validExtensions.includes(fileType)){ //if user selected file is an image file
-    let fileReader = new FileReader(); //creating new FileReader object
-    fileReader.onload = ()=>{
-      let fileURL = fileReader.result; //passing user file source in fileURL variable
-      let imgTag = `<img src="${fileURL}" alt="image" height=500 width=500>`; //creating an img tag and passing user selected file source inside src attribute
+  let fileType = file.type; 
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; 
+  
+  if(validExtensions.includes(fileType)) { 
+    let fileReader = new FileReader(); 
+    fileReader.onload = () => {
+      //passing user file source in fileURL variable
+      let fileURL = fileReader.result; 
+      //creating an img tag and passing user selected file source inside src attribute
+      let imgTag = `<img src="${fileURL}" alt="image" height=500 width=500>`; 
       dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
     }
+
     fileReader.readAsDataURL(file);
-    //show generated caption, once the image is uploaded and shown
+    // show generated caption, once the image is uploaded and shown
     document.getElementById('caption').append('Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime esse asperiores tempora dicta commodi, nesciun')
-    //show a button to remove the image 
+    // show a button to remove the image 
     removeImageButton.style.display = "block";
 
   } else {
